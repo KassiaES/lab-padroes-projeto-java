c3af3280fc6ecf7857d34fa47660af6902bb2c23
Kassia do Espirito Santo <kassiaes@gmail.com>
2023-02-20 17:18:31 -0300

    padrões iniciais

---
+++ src/Test.java
@@ -0,0 +1,46 @@
+import facade.Facade;
+import singleton.SingletonEager;
+import singleton.SingletonLazy;
+import singleton.SingletonLazyHolder;
+import strategy.*;
+
+public class Test {
+
+    public static void main(String[] args) {
+
+        // Testes relacionados ao Design Pattern Singleton
+
+        SingletonLazy lazy = SingletonLazy.getInstancia();
+        System.out.println(lazy);
+        lazy = SingletonLazy.getInstancia();
+        System.out.println(lazy);
+
+        SingletonEager eager = SingletonEager.getInstancia();
+        System.out.println(eager);
+        eager = SingletonEager.getInstancia();
+        System.out.println(eager);
+
+        SingletonLazyHolder holder = SingletonLazyHolder.getInstancia();
+        System.out.println(holder);
+        holder = SingletonLazyHolder.getInstancia();
+        System.out.println(holder);
+
+        // Strategy
+        Comportamento defensivo = new ComportamentoDefensivo();
+        Comportamento normal = new ComportamentoNormal();
+        Comportamento agressivo = new ComportamentoAgressivo();
+
+        Robo robo = new Robo();
+        robo.setComportamento(normal);
+        robo.mover();
+        robo.setComportamento(defensivo);
+        robo.mover();
+        robo.setComportamento(agressivo);
+        robo.mover();
+
+        // Facade
+        Facade facade = new Facade();
+        facade.migrarCliente("Kassia", "09210650");
+
+    }
+}
